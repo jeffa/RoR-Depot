981160b5529449f1dfd8453b5fba614a40ea2e7e
jeffa <jeffa@unlocalhost.com>
2013-05-12 00:42:08 -0400

    added seeds; added Bootstrap stylings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+
+Product.create(
+  title: 'foo',
+  description: %{<p>something about foo</p>},
+  image_url: 'foo.jpg',
+  price: 19.95
+)
+
+Product.create(
+  title: 'bar',
+  description: %{<p>something about bar</p>},
+  image_url: 'bar.jpg',
+  price: 29.95
+)
+
+Product.create(
+  title: 'baz',
+  description: %{<p>something about baz</p>},
+  image_url: 'baz.jpg',
+  price: 39.95
+)
+
+Product.create(
+  title: 'qux',
+  description: %{<p>something about qux</p>},
+  image_url: 'qux.jpg',
+  price: 49.95
+)
